9171d6b239a4da459850bf3f1425d49e78a836b9
OmKumb11 <omkumbhar1111@gmail.com>
2026-01-04 13:35:45 +0530

    Upgraded model to use Multiple Linear Regression (Runs + Wickets)

--- predict_score.py
+++ predict_score.py
@@ -4,15 +4,15 @@ from sklearn.linear_model import LinearRegression
 
 data = pd.read_csv('cricket_data.csv')
 
-X = data[['powerplay_runs']]
+X = data[['powerplay_runs', 'wickets']]
 Y = data['final_score']
 
 model = LinearRegression()
 model.fit(X,Y)
 
-print("IPL Score Predictor")
-user_in = int(input("Enter the Number of Runs Scored in Powerplay (6 Overs):  "))
-
-prediction = model.predict([[user_in]])
+print("IPL Score Predictor 2.0")
+user_in = float(input("Enter the Number of Runs Scored in Powerplay (6 Overs):  "))
+wickets = float(input("Wickets lost: "))
+prediction = model.predict([[user_in,wickets]])
 
 print(f"Predicted Final Score: {int(prediction[0])}")
